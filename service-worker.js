c73e380949a6145e6d8a705b2d4f542aa4ae4c5a
Amir Mohammad Hedayati Nia <84AmirMohammad.h.n@gmail.com>
2026-09-10 17:25:39 +0330

    Update Tic Tac Toe v2.1.2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this version any time cached files change to force an update.
-const CACHE_NAME = 'tic-tac-toe-v2.1.1';
+const CACHE_NAME = 'tic-tac-toe-v2.1.2';
 
 // Local app-shell files (same-origin)
 const ASSETS_TO_CACHE = [
